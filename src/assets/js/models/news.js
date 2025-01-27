476f7bd58b54732e2b650779b67c685a6eb60478
Murilo Maciel Rodrigues <murilomacielbr@gmail.com>
2025-01-27 19:43:41 -0300

    another fix

--- src/assets/js/models/news.js
+++ src/assets/js/models/news.js
@@ -5,7 +5,7 @@ const news =
         titulo: 'Rockstar Games Revela GTA 6 com Ambientação Inspirada em Vice City e Los Santos',
         subtitulo: 'Nova edição promete ser a maior de todas, com mapa expansivo e novas mecânicas de jogabilidade.',
         img: 'images/news/GTAV.png', 
-        link: '/src/assets/pages/news/gta.html'
+        link: 'src/assets/pages/news/gta.html'
         
     },
 
@@ -14,7 +14,7 @@ const news =
         titulo: 'Blizzard Anuncia Novo Evento de Temporada e Modo PvE Exclusivo para Overwatch 2',
         subtitulo: '“Operação Aurora” trará missões cooperativas e recompensas especiais por tempo limitado.',
         img: 'images/news/Overwatch.png', 
-        link: '/src/assets/pages/news/overwatch.html'
+        link: 'src/assets/pages/news/overwatch.html'
     },
 
     {
@@ -22,7 +22,7 @@ const news =
         titulo: 'Valorant Lança Modo de Jogo Inédito “Missão Tempestade”',
         subtitulo: 'Atualização traz missões cooperativas, narrativa expandida e novos mapas ambientados em locais exóticos.',
         img: 'images/news/Valorant.png', 
-        link: '/src/assets/pages/news/valorant.html'
+        link: 'src/assets/pages/news/valorant.html'
     }
 ]
 
