2d5eec6b2056219c393b61500ad36015471138f9
Felipe Oliveira Raimundo <20241370007@ad.ifpb.edu.br>
2025-01-27 19:09:36 -0300

    criação de reviews e noticias

--- src/assets/js/lib/lib.js
+++ src/assets/js/lib/lib.js
@@ -5,3 +5,14 @@ export function exibir(link) {
 export function converter_link_imagem(img){
     return '../' + img.substring(11)
 }
+
+export function header_link(){
+    if (localStorage.length === 0 || localStorage.getItem('user') === 'none') {
+        document.getElementById('header_link').insertAdjacentHTML('beforeend', '<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/login.html">Login</a>')
+    } 
+    else {
+        let userInstance = localStorage.getItem('user')
+        let user = JSON.parse(localStorage.getItem(userInstance))
+        document.getElementById('header_link').insertAdjacentHTML('beforeend', `<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/profile.html"> <i class="fa-solid fa-user"></i> ${user.nome_usuario} </a>`)
+    }
+}
--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,9 +1,12 @@
 //elemento dinâmico botão de login
-if (localStorage.length === 0 || localStorage.getItem('user') === 'none') {
-    document.getElementById('header_link').insertAdjacentHTML('beforeend', '<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/login.html">Login</a>')
-} 
-else {
-    let userInstance = localStorage.getItem('user')
-    let user = JSON.parse(localStorage.getItem(userInstance))
-    document.getElementById('header_link').insertAdjacentHTML('beforeend', `<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/profile.html"> <i class="fa-solid fa-user"></i> ${user.nome_usuario} </a>`)
-}
+import * as lib from './lib/lib.js'
+// if (localStorage.length === 0 || localStorage.getItem('user') === 'none') {
+//     document.getElementById('header_link').insertAdjacentHTML('beforeend', '<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/login.html">Login</a>')
+// } 
+// else {
+//     let userInstance = localStorage.getItem('user')
+//     let user = JSON.parse(localStorage.getItem(userInstance))
+//     document.getElementById('header_link').insertAdjacentHTML('beforeend', `<a class="rounded-md bg-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow" href="src/assets/pages/profile.html"> <i class="fa-solid fa-user"></i> ${user.nome_usuario} </a>`)
+// }
+
+lib.header_link()
